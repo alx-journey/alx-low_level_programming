4e4d454f8da490ff446736dd705feed02a313eae
macbook <user@macbooks-MacBook-Pro.local>
2023-08-11 05:55:57 +0100

    size it

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,10 +8,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s)\n", sizeof(char));
-	printf("Size of an int: %lu byte(s)\n", sizeof(int));
-	printf("Size of long int: %lu byts(s)\n", sizeof(long));
-	printf("Size of a long long int : %lu byte(s)\n", sizeof(long long));
-	printf("Size of a float: %lu byte(s)\n", sizeof(float));
+	printf("Size of a char: %zu byte(s)\n", sizeof(char));
+	printf("Size of an int: %zu byte(s)\n", sizeof(int));
+	printf("Size of long int: %zu byts(s)\n", sizeof(long));
+	printf("Size of a long long int : %zu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }

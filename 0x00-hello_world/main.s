2d5ab638a520d6a52316b082d539714319e5fd1b
macbook <user@macbooks-MacBook-Pro.local>
2023-08-11 05:01:10 +0100

    intel

---
+++ 0x00-hello_world/main.s
@@ -0,0 +1,24 @@
+	.section	__TEXT,__text,regular,pure_instructions
+	.macosx_version_min 10, 12
+	.intel_syntax noprefix
+	.globl	_main
+	.p2align	4, 0x90
+_main:                                  ## @main
+	.cfi_startproc
+## BB#0:
+	push	rbp
+Lcfi0:
+	.cfi_def_cfa_offset 16
+Lcfi1:
+	.cfi_offset rbp, -16
+	mov	rbp, rsp
+Lcfi2:
+	.cfi_def_cfa_register rbp
+	xor	eax, eax
+	mov	dword ptr [rbp - 4], 0
+	pop	rbp
+	ret
+	.cfi_endproc
+
+
+.subsections_via_symbols

75235dbaa78d8a01ee8b23a88e928ec54acaac31
macbook <user@macbooks-MacBook-Pro.local>
2023-08-11 04:23:47 +0100

    sizeof function

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return 0 when successful
+*/
+
+int main(void)
+{
+	printf("size of a char: %ch byte(s)", sizeof(char));
+	printf("size of int: %ch byte(s)", sizeof(int));
+	printf("size of long int: %ch byts(s)", sizeof(long int);
+	printf ("size of a long int : byte(s)", sizeof(long long int));
+	printf("size of a float: %ch byte(s", sizeof(float));
+	return (0);
+}

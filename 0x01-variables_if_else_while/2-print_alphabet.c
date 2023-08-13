553a85b072c3b19156497a6d36187a29bd9a12f3
macbook <user@macbooks-MacBook-Pro.local>
2023-08-13 02:28:59 +0100

    alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* return: always 0
+*/
+int main(void)
+{
+	char i;
+	for (i = 'a' ; i <= 'z' ; i++)
+		putchar(i);
+	printf("\n");
+	return (0);
+}

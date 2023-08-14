63e0c3344635b2919f7759a999391fd5a467faf0
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 02:30:11 +0100

    reverse char

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+int main(void)
+{
+
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}

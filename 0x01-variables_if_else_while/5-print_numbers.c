243cd54130120f370b159b42ba4a75961e8aa4e8
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 01:41:40 +0100

    print numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		printf("%d \n", i);
+
+	return (0);
+
+}

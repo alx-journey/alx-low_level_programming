11fbd984efbb05ca6fe9aa20715892a7a6ce81e7
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 02:49:25 +0100

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+int main(void)
+{
+
+	char i;
+	int j;
+
+	for (j = 0 ; j < 10 ; j++)
+		putchar(j + '0');
+	for (i = 'a' ; i <= 'f' ; i++)
+		putchar(i);
+	putchar('\n');
+	return (0);
+
+}

2a45f1d329328e21fda98698c65f8d31bd720e5b
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 02:02:22 +0100

    outputting converted int to char

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		putchar(i + '0');
+	printf("\n");
+	return (0);
+
+}

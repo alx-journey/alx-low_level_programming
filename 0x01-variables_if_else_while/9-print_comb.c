7a8ae49d2bcd44118f774af716a379bb530741cb
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 03:29:11 +0100

    print combo

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: return zero
+*
+*/
+
+int main(void)
+{
+
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+	{
+
+		if (i == 9)
+		{
+			putchar(i + '0');
+		}
+		else
+		{
+			putchar(i + '0');
+			putchar(',');
+			putchar(' ');
+		}
+
+	}
+	putchar('\n');
+	return (0);
+
+}

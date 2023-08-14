6e8f4137c3f649cb242e70a92df291a96bb2e3cc
macbook <user@macbooks-MacBook-Pro.local>
2023-08-14 19:19:36 +0100

    printing two two

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i < 9 ; i++)
+	{
+		for (j = 1 ; j <= 9 ; j++)
+			if (!(i >= j))
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+
+				if (i != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+
+	}
+	putchar('\n');
+	return (0);
+}

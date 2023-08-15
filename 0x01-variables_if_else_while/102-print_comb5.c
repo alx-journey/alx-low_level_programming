044385e579791c93c9d0629b49e710c723542138
macbook <user@macbooks-MacBook-Pro.local>
2023-08-15 06:01:11 +0100

    double

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+	int i, j;
+	int a, b, c, d;
+
+	for (i = '0' ; i <= 99 ; i++)
+	{
+		a = i / 10;
+		b = i % 10;
+	
+		for (j = '0' ; j <= 99 ; j++)
+		{
+			c = j / 10;
+			d = j % 10;
+			if (a < c || (a == c && b < d))
+			{
+				putchar(a + '0');
+				putchar(b + '0');
+				putchar(' ');
+				putchar(c + '0');
+				putchar(d + '0');
+
+				if (a != '9' && b != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

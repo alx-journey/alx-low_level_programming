72ab80d158c1bbb184ad0ce16081f63cbde66201
macbook <user@macbooks-MacBook-Pro.local>
2023-08-15 06:09:03 +0100

    double

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,12 +11,11 @@ int main(void)
 	int i, j;
 	int a, b, c, d;
 
-	for (i = '0' ; i <= 99 ; i++)
+	for (i = 0 ; i <= 99 ; i++)
 	{
 		a = i / 10;
 		b = i % 10;
-	
-		for (j = '0' ; j <= 99 ; j++)
+		for (j = 0 ; j <= 99 ; j++)
 		{
 			c = j / 10;
 			d = j % 10;

f528843f112f76341872edd20915815c534a383c
macbook <user@macbooks-MacBook-Pro.local>
2023-08-15 04:44:20 +0100

    print three three

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: always return 0
+*/
+
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; i < 9 ; i++)
+	{
+	for (j = i + 1 ; j <= 9 ; j++)
+	{
+	for (k = j + 1 ; k <= 9 ; k++)
+	{
+	if ((j != i) != k)
+	{
+		putchar(i + '0');
+		putchar(j + '0');
+		putchar(k + '0');
+
+	if (i == 7 && j == 8)
+	continue;
+	putchar(',');
+	putchar(' ');
+	}
+	}
+	}
+	}
+	putchar('\n');
+	return (0);
+}

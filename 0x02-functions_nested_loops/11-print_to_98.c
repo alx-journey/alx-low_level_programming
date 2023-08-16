7875e191ec848e4f39bc3d6e07e3fae01c6f20dc
macbook <user@macbooks-MacBook-Pro.local>
2023-08-16 06:16:57 +0100

    battery

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* print_to_98 - function that check for lowercase and uppercase
+* @n: parameter a to be checked
+* Return: void
+*/
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n >= 0 && n <= 98)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n < 0)
+	{
+		for (i = n ; i <= 98 ; i++)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+	else if (n > 98)
+	{
+		for (i = n ; i >= 98 ; i--)
+		{
+			printf("%d", i);
+		}
+		printf("\n");
+	}
+
+}

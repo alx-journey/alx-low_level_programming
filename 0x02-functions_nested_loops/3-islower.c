6ec1c4c2efe19b6abe33da08fe60286d8b5eaa40
macbook <user@macbooks-MacBook-Pro.local>
2023-08-16 03:23:48 +0100

    is lower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _islower - function that check for lowercase
+* @c: parameter to be checked
+* Return: always (0)
+*/
+
+int _islower(int c)
+{
+
+	if (c >= 'a' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}

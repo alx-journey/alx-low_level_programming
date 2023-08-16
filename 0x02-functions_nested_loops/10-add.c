e7d8017c98ca95cdc6bd7c7df257ba2b7eedbff3
macbook <user@macbooks-MacBook-Pro.local>
2023-08-16 05:59:52 +0100

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* add - function that check for lowercase and uppercase
+* @a: parameter a to be checked
+* @b: parameter b to be checked
+* Return: a + b
+*/
+
+int add(int a, int b)
+{
+
+	return (a + b);
+}

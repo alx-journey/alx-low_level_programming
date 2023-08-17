9a8d6ed216300675a5f32dd2b01147899038c794
macbook <user@macbooks-MacBook-Pro.local>
2023-08-17 22:57:53 +0100

    add two integer

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* mul - function checking for uppercase
+*@a: parameter defined
+*@b: parameter defined
+* Return: a * b
+*/
+
+int mul(int a, int b)
+{
+
+	return (a * b);
+
+
+}

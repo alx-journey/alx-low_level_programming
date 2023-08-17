00a2c64ada73926d948a8ae957fd05776915c1f8
macbook <user@macbooks-MacBook-Pro.local>
2023-08-17 05:59:24 +0100

    multiples

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+* main - main function
+*
+* Return: zero
+*/
+int main(void)
+{
+	int sum = 0;
+	int i;
+
+	for (i = 0 ; i < 1024 ; i++)
+	{
+		if (i % 3 == 0 || i % 5 == 0)
+			sum = sum + i;
+	}
+	printf("%d\n", sum);
+	return (0);
+}

84416b523751e6f0a4b5230686dc722540db4e61
macbook <user@macbooks-MacBook-Pro.local>
2023-08-18 01:19:17 +0100

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+* more_numbers - function to print 0 to 14, 10times
+*
+* Return: void
+*/
+
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14; j++)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+
+		}
+
+		_putchar('\n');
+
+	}
+
+
+
+}

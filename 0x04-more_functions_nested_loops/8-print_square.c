03c7b699b0b012d9fb31e8267decbfd93fc82cba
macbook <user@macbooks-MacBook-Pro.local>
2023-08-18 03:57:02 +0100

    square

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+* print_square - function to print diagonal characters
+*@size: parameter n passed
+* Return: void
+*/
+
+void print_square(int size)
+{
+
+	if (size <= 0)
+		_putchar('\n');
+
+	else
+	{
+		int i, j;
+
+		for (i = 1 ; i <= size ; i++)
+		{
+			for (j = 0 ; j <= size ; j++)
+				_putchar('#');
+			_putchar('\n');
+		}
+
+	}
+}

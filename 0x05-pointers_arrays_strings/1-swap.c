9db3b1ca82612fb98dd189616281a6071bc75255
macbook <user@macbooks-MacBook-Pro.local>
2023-08-22 01:12:04 +0100

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* swap_int - resets n to 98
+* @a: pointer to the value to be swaped
+* @b: pointer to the value to be swaped
+* Return: void
+*/
+
+void swap_int(int *a, int *b)
+{
+
+	int c;
+
+	c = *a;
+	*a = *b;
+	c = *a;
+}

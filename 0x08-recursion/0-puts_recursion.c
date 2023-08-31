6b90b23ec4ec0babcb9476b19239b196777eecdf
macbook <user@macbooks-MacBook-Pro.local>
2023-08-31 05:00:25 +0100

    print strings with recursive function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* _puts_recursion - recursive function to print a string
+*
+* @s: special variable or parameter called pointer
+*
+*Return: void
+*/
+
+void _puts_recursion(char *s)
+{
+
+	if (*s != '\0')
+	{
+		_putchar(*s);
+		_puts_recursion(s + 1);
+	}
+	else if (*s == '\0')
+		_putchar('\n');
+
+}

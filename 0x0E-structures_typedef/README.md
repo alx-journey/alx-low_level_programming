fc115d5038df96437a7f6a94bebbd4384fdf63e6
macbook <user@macbooks-MacBook-Pro.local>
2023-09-12 04:11:04 +0100

    dog desc in struct

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,1 @@
+structure type def in c
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+* struct dog - dog structure
+* @name: name of a dog
+* @age: age of dog
+* @owner: name of owner
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+
+};
+
+#endif

58a7ef7be1f6b07ee4ad9d23718f2f710e57af9e
macbook <user@macbooks-MacBook-Pro.local>
2023-12-23 03:20:38 +0100

    task3 fixed

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,19 +1,26 @@
 #include "hash_tables.h"
+
 /**
- * hash_table_get - function to retrieve value associeted with key
+ * hash_table_set - function that adds an element to the hash table
  * @ht: pointer to hash table
- * @key: key to retrive value
+ * @key: key to add the element
+ * @value: value to add the element
  *
- * Return: value or NULL if key does not exit.
+ * Return: 1 if it succeeded, 0 otherwise
  */
 
-char *hash_table_get(const hash_table_t *ht, const char *key)
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index = 0;
-	hash_node_t  *bucket;
+	char *valuecopy, *keycopy;
+	hash_node_t  *bucket, *new_node;
 
-	if (!ht || !key || !*key)
-		return (NULL);
+	if (!ht || !key || !*key || !value)
+		return (0);
+
+	valuecopy = strdup(value);
+	if (!valuecopy)
+		return (0);
 
 	index = key_index((const unsigned char *)key, ht->size);
 	bucket = ht->array[index];
@@ -21,9 +28,25 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	while (bucket)
 	{
 		if (!strcmp(key, bucket->key))
-			return (bucket->value);
+		{
+			free(bucket->value);
+			bucket->value = valuecopy;
+			return (1);
+		}
 		bucket = bucket->next;
 	}
-	return (NULL);
+	new_node = calloc(1, sizeof(hash_node_t));
+	if (new_node == NULL)
+	{
+		free(valuecopy);
+		return (0);
+	}
+	keycopy = strdup(key);
+	if (!keycopy)
+		return (0);
+	new_node->key = keycopy;
+	new_node->value = valuecopy;
+	new_node->next = ht->array[index];
+	ht->array[index] = new_node;
+	return (1);
 }
-
